6cc3ba878216cafb38b77af4c7921f45759f7eeb
Paulo Coelho <pdpcoelho@gmail.com>
2023-10-27 00:03:24 +0100

    alteração stack

--- stack.c
+++ stack.c
@@ -1,84 +1,81 @@
 #include "stack.h"
 
 struct stack_ {
-  int top;
-  unsigned capacity;
-  Item* array;
+	int top;
+	unsigned capacity;
+	Item* array;
 };
 
 /* function to create a stack of given capacity; initializes size as 0 */
 Stack* createStack(unsigned capacity)
 {
-    Stack* stack = (Stack*)malloc(sizeof(Stack));
-    stack->capacity = capacity;
-    /* unused so far */
-    stack->top = -1;
-    stack->array = (Item*) malloc(stack->capacity * sizeof(Item));
-    return stack;
+	Stack* stack = (Stack*)malloc(sizeof(Stack));
+	stack->capacity = capacity;
+	/* unused so far */
+	stack->top = -1;
+	stack->array = (Item*)malloc(stack->capacity * sizeof(Item));
+	return stack;
 }
 
 
 /* Stack is full when top is equal to the last index */
 int isFull(Stack* stack)
 {
-    return stack->top == stack->capacity - 1;
+	return stack->top == stack->capacity - 1;
 }
 
 
 /* Stack is empty when top is equal to -1 */
 int isEmpty(Stack* stack)
 {
-  return stack->top == -1;
+	return stack->top == -1;
 }
 
 
 /* Function to add an item to stack.  It increases top by 1 */
 void push(Stack* stack, Item item)
 {
-  /* cannot add if it is full */
-  if (isFull(stack))
-    return;
-  stack->array[++stack->top] = item;
-
-  return;
+	/* cannot add if it is full */
+	if (!isFull(stack))
+		stack->array[++stack->top] = item;
 }
 
 
 /* Function to remove an item from stack.  It decreases top by 1 */
 Item pop(Stack* stack)
 {
-  /* nothing to return if it is empty */
-  if (isEmpty(stack))
-    return NULL;
+	/* nothing to return if it is empty */
+	if (isEmpty(stack))
+		return NULL;
 
-  Item poppedItem = stack->array[stack->top--];
+	Item poppedItem = stack->array[stack->top--];
 
-  return(poppedItem);
+	return(poppedItem);
 }
 
 
 /* Function to return the top from stack without removing it */
 Item peekFisrt(Stack* stack, int index)
 {
-  if (isEmpty(stack))
-    return NULL;
+	if (isEmpty(stack))
+		return NULL;
 
-  return stack->array[index];
+	return stack->array[index];
 }
 
 
 /* function to delete all elements form stack */
-void deleteStack(Stack *stack)
+void deleteStack(Stack* stack)
 {
-  /* note it does not delete the elements, just the stack itself */
-  //free(stack->array->)
-  free (stack->array);
-  free(stack);
+	/* note it does not delete the elements, just the stack itself */
+	//free(stack->array->)
+	free(stack->array);
+	free(stack);
 
-  return;
+	return;
 }
 
-int getTop(Stack *stack)
+int getTop(Stack* stack)
 {
-  return stack->top;
+	return stack->top;
 }
